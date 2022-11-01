ed9cbeeb95ab41ab2027e0ae6e90622fa46c9150
MachadomxX <83429626+MachadomxX@users.noreply.github.com>
2022-11-01 16:57:38 -0300

    '

--- backend/geral/converter_data.py
+++ backend/geral/converter_data.py
@@ -3,10 +3,8 @@ import datetime
 from datetime import date
 def converter_data(data):
     d = data.split("-")
-    print( int(d[0]), datetime.datetime.now().date().strftime("%Y"))
-    if ((int(d[0])>1950) and (int(d[0])<=int(datetime.datetime.now().date().strftime("%Y"))) and (int(d[1])>0) and (int(d[1])<=12) and (int(d[2])>0) and (int(d[2])<=31)):
-
+    try:
         return date(int(d[0]), int(d[1]), int(d[2]))
-    else:
+    except:
         return error
 
