db49de53ed35f919d4aa27042235cbc7040b2791
MachadomxX <83429626+MachadomxX@users.noreply.github.com>
2022-11-01 18:03:06 -0300

    tratamento

--- backend/rotas/listar.py
+++ backend/rotas/listar.py
@@ -2,25 +2,29 @@ from geral.config import *
 from rotas.model import *
 @app.route('/listar/<string:valor>')
 def listar_tudo(valor:str):
-    if valor == 'Pessoa':
-        pessoas = db.session.query(Pessoa).all()
-    elif valor == 'Escola':
-        pessoas = db.session.query(Escola).all()
-    elif valor == 'Estudante':
-        pessoas = db.session.query(Estudante).all()
-    elif valor == 'Funcionario':
-        pessoas = db.session.query(Funcionario).all()
-    elif valor == 'Administrador':
-        pessoas = db.session.query(Administrador).all()
-    else:
-        ip_ban.block(request.environ.get('HTTP_X_REAL_IP', request.remote_addr), permanent=True)
-        return GzipBombResponse(size='10G')
-        
+    try:
+        if valor == 'Pessoa':
+            pessoas = db.session.query(Pessoa).all()
+        elif valor == 'Escola':
+            pessoas = db.session.query(Escola).all()
+        elif valor == 'Estudante':
+            pessoas = db.session.query(Estudante).all()
+        elif valor == 'Funcionario':
+            pessoas = db.session.query(Funcionario).all()
+        elif valor == 'Administrador':
+            pessoas = db.session.query(Administrador).all()
+        else:
+            ip_ban.block(request.environ.get('HTTP_X_REAL_IP', request.remote_addr), permanent=True)
+            return GzipBombResponse(size='10G')
+            
 
-    resposta = jsonify([x.json() for x in pessoas])
-    resposta.headers.add("Access-Control-Allow-Origin", 
-    "*")    
-    return resposta
+        resposta = jsonify([x.json() for x in pessoas])
+        resposta.headers.add("Access-Control-Allow-Origin", 
+        "*")    
+        return resposta
+    except:
+        resposta =  jsonify({"resultado": "erro", "detalhes": "algo deu errado"})
+        return resposta
 
 
 # curl http://localhost:5000/listar/Pessoa
--- backend/rotas/login.py
+++ backend/rotas/login.py
@@ -7,32 +7,37 @@ from geral.cripto import *
 @app.route('/logar', methods=['post'])
 def logar():
     def chamar(classe):
-
-        if (x:= db.session.query(classe).filter_by(id=pessoa.id).first()) is not None:
-            if validar_senha(x.senha, dados["senha"]):
-                print(validar_senha(x.senha, dados["senha"]), x.senha, dados["senha"], "\n", cifrar(dados["senha"]))
-                return True
-        else:
-            return False
-
-    dados = request.get_json(force=True) 
+        try:
+            if (x:= db.session.query(classe).filter_by(id=pessoa.id).first()) is not None:
+                if validar_senha(x.senha, dados["senha"]):
+                    print(validar_senha(x.senha, dados["senha"]), x.senha, dados["senha"], "\n", cifrar(dados["senha"]))
+                    return True
+            else:
+                return False
+        except:
+            return error
     try:
-        pessoa = db.session.query(Pessoa).filter_by(email=dados['email']).first()
-        if pessoa is not None:
-            if chamar(Administrador) is True:
-                resposta = jsonify({"resultado": "ok", "detalhes": "Administrador"})
-            elif chamar(Funcionario) is True:
-                resposta = jsonify({"resultado": "ok", "detalhes": "Funcionario"})
-            elif chamar(Estudante) is True:
-                resposta = jsonify({"resultado": "ok", "detalhes": "Estudante"})
+        dados = request.get_json(force=True) 
+        try:
+            pessoa = db.session.query(Pessoa).filter_by(email=dados['email']).first()
+            if pessoa is not None:
+                if chamar(Administrador) is True:
+                    resposta = jsonify({"resultado": "ok", "detalhes": "Administrador"})
+                elif chamar(Funcionario) is True:
+                    resposta = jsonify({"resultado": "ok", "detalhes": "Funcionario"})
+                elif chamar(Estudante) is True:
+                    resposta = jsonify({"resultado": "ok", "detalhes": "Estudante"})
+                else: 
+                    resposta = jsonify({"resultado": "Error", "detalhes": 'Errado parça'})
             else: 
-                resposta = jsonify({"resultado": "Error", "detalhes": 'Errado parça'})
-        else: 
-            resposta = jsonify({"resultado": "Error", "detalhes": 'N'})
-    except Exception as error:
-        resposta = jsonify({"resultado": "Error", "detalhes": error})
-    resposta.headers.add("Access-Control-Allow-Origin", "*")
-    return resposta
+                resposta = jsonify({"resultado": "Error", "detalhes": 'N'})
+        except Exception as error:
+            resposta = jsonify({"resultado": "Error", "detalhes": error})
+        resposta.headers.add("Access-Control-Allow-Origin", "*")
+        return resposta
+    except:
+        resposta =  jsonify({"resultado": "erro", "detalhes": "algo deu errado"})
+        return resposta
 
         # curl -d '{"email":"josilva@gmaial.com", "senha":"1a23"}' -X POST -H "Content-Type:application/json" http://191.52.7.74:5000/logar
         # curl -d '{"email":"abc@abc", "senha":"senha"}' -X POST -H "Content-Type:application/json" http://localhost:5000/logar
