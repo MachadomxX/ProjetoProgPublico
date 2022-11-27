7a7de2a783d1ebc893bb2a701277e5ab9e62f4f3
bwlarissa <101813397+bwlarissa@users.noreply.github.com>
2022-11-27 14:15:53 -0300

    Add files via upload

---
+++ frontend/email.php
@@ -0,0 +1,14 @@
+<?php
+
+if(isset($_POST(['email']) && !empty($_POST(['email']))
+$nome = addcslashes($_POST(['name']))
+$email = addcslashes($_POST(['email']))
+$mensagem = addcslashes($_POST(['message']))
+
+$to = "warmlinglarissa1@gmail.com";
+$subject = "Contato = Programa";
+$body = "Nome:".$nome. ""
+
+}
+
+?>
